fb83efa4c07d6376495f3fd92e1cc4ee4057c768
Andreas Kunar <andreask@msn.com>
2023-06-04 13:45:58 +0200

    enabling docx in ingest (imartinez comment-out)

--- ingest.py
+++ ingest.py
@@ -18,6 +18,7 @@ from langchain.document_loaders import (
     UnstructuredODTLoader,
     UnstructuredPowerPointLoader,
     UnstructuredWordDocumentLoader,
+    Docx2txtLoader,
 )
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -64,7 +65,7 @@ class MyElmLoader(UnstructuredEmailLoader):
 # Map file extensions to document loaders and their arguments
 LOADER_MAPPING = {
     ".csv": (CSVLoader, {}),
-    # ".docx": (Docx2txtLoader, {}),
+    ".docx": (Docx2txtLoader, {}),
     ".doc": (UnstructuredWordDocumentLoader, {}),
     ".docx": (UnstructuredWordDocumentLoader, {}),
     ".enex": (EverNoteLoader, {}),
